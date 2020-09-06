eb867bbb51af44f792681a770ab1f99fef558ca1
Adit Modi <adit.modi24@gmail.com>
2020-09-06 09:11:53 +0530

    Update K Means Clustering with Python.py

--- K Means Clustering with Python.py
+++ K Means Clustering with Python.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 # ## Method Used
 # 
 # K Means Clustering is an unsupervised learning algorithm that tries to cluster data based on their similarity. Unsupervised learning means that there is no outcome to be predicted, and the algorithm just tries to find patterns in the data. In k means clustering, we have the specify the number of clusters we want the data to be grouped into. The algorithm randomly assigns each observation to a cluster, and finds the centroid of each cluster. Then, the algorithm iterates through two steps:
